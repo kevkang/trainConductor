69f4e0b55f081e1d2d6b21881e4fff2a68a25fd0
Kevin Kang <kkang@berkeley.edu>
2016-02-13 03:53:13 -0800

    added preliminary program

---
+++ myNewMyoApp.py
@@ -0,0 +1,18 @@
+import myo as libmyo
+libmyo.init('C:/Users/Kevin/Documents/treehacks/notTree/myo-sdk-win-0.9.0/bin')
+feed = libmyo.device_listener.Feed()
+hub = libmyo.Hub()
+hub.run(1000, feed)
+try:
+    myo = feed.wait_for_single_device(timeout=10.0)  # seconds
+    if not myo:
+        print("No Myo connected after 10 seconds.")
+        sys.exit()
+
+    while hub.running and myo.connected:
+        quat = myo.orientation
+        print("Orientation:", quat.x, quat.y, quat.z, quat.w)
+except KeyboardInterrupt:
+    print("Quitting...")
+finally:
+    hub.shutdown()
