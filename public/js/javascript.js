bf34eec03920b8942774a7a6855c04fd522a73e2
Matthew Zhu <matt.z496@gmail.com>
2016-02-14 12:02:23 -0500

    check and x

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -16,86 +16,88 @@ gyroItems.threshold = 20;
 
 //repeats every 10 ms to compare against prevState
 function gyroRepeat(data) {
-    gyroItems.prevState = gyroItems.state;
-    if ((Math.abs(data.z) > Math.abs(data.y)) && (data.z > gyroItems.threshold)) {
-        gyroItems.state = "left";
-    } else if ((Math.abs(data.y) > Math.abs(data.z)) && (data.y > gyroItems.threshold)) {
-        gyroItems.state = "up";
-    } else if ((Math.abs(data.z) > Math.abs(data.y)) && (data.z < -gyroItems.threshold)) {
-        gyroItems.state = "right";
-    } else if ((Math.abs(data.y) > Math.abs(data.z)) && (data.y < -gyroItems.threshold)) {
-        gyroItems.state = "down";
-    }
-    if (gyroItems.prevState != gyroItems.state) {
-        if (!gyroItems.receivedBeat && 
-            (gyroItems.prevState == 
-            gyroItems.selectedArray[gyroItems.beatNumber %
-            gyroItems.selectedArray.length])) {
-            gyroItems.receivedBeat = true;
-            //send BEAT
-            console.log("beat " + gyroItems.beatNumber +
-                gyroItems.selectedArray[gyroItems.beatNumber %
-                gyroItems.selectedArray.length], gyroItems.state);
-            beat();
-        }
-        if (gyroItems.state == gyroItems.selectedArray[
-            (gyroItems.beatNumber + 1) % gyroItems.selectedArray.length]) {
-            gyroItems.beatNumber++;
-            gyroItems.receivedBeat = false;
-        }
-    }
+  gyroItems.prevState = gyroItems.state;
+  if ((Math.abs(data.z) > Math.abs(data.y)) && (data.z > gyroItems.threshold)) {
+      gyroItems.state = "left";
+  } else if ((Math.abs(data.y) > Math.abs(data.z)) && (data.y > gyroItems.threshold)) {
+      gyroItems.state = "up";
+  } else if ((Math.abs(data.z) > Math.abs(data.y)) && (data.z < -gyroItems.threshold)) {
+      gyroItems.state = "right";
+  } else if ((Math.abs(data.y) > Math.abs(data.z)) && (data.y < -gyroItems.threshold)) {
+      gyroItems.state = "down";
+  }
+  if (gyroItems.prevState != gyroItems.state) {
+      if (!gyroItems.receivedBeat && 
+          (gyroItems.prevState == 
+          gyroItems.selectedArray[gyroItems.beatNumber %
+          gyroItems.selectedArray.length])) {
+          gyroItems.receivedBeat = true;
+          //send BEAT
+          console.log("beat " + gyroItems.beatNumber +
+              gyroItems.selectedArray[gyroItems.beatNumber %
+              gyroItems.selectedArray.length], gyroItems.state);
+          beat();
+      }
+      if (gyroItems.state == gyroItems.selectedArray[
+          (gyroItems.beatNumber + 1) % gyroItems.selectedArray.length]) {
+          gyroItems.beatNumber++;
+          gyroItems.receivedBeat = false;
+      }
+  }
 }
 
 function beat() {
-    $("#otherCircle").show();
-    setTimeout(function() {
-        $("#otherCircle").hide()
-    }, 100);
-    if (holder != 0) {
-        pushOntoMe.push(holder - Date.now())
-    }
-    holder = Date.now();
+  var modMe = gyroItems.beatNumber;
+  $("#otherCircle").show();
+  setTimeout(function() {
+      $("#otherCircle").hide()
+  }, 100);
+  if (holder != 0) {
+      pushOntoMe.push(holder - Date.now())
+  }
+  holder = Date.now();
+  var arrowNum = (modMe % 4) + 1;
+  var myArrow = ".arrow" + arrowNum.toString();
+  if ($(myArrow).css("color") != "rgb(158, 158, 158)") {
+    $(".correct").css("color", "#66bb6a");
+    $(".incorrect").css("color", "");
+  } else {
+    $(".correct").css("color", "");
+    $(".incorrect").css("color", "#ef5350");
+  }
 }
 
 function addEvents(myo) {
-    $(".conduct-btn").click(function() {
-        setTimeout(function() {
-            if (!conduct) {
-                conduct = true;
-                myo.on('gyroscope',function(data) {
-                    setTimeout(gyroRepeat(data),10);
-                });
-            } else {
-                conduct = false;
-                myo.off();
-                gyroItems.prevState;
-                gyroItems.receivedBeat;
-                gyroItems.beatNumber = 0;
-                gyroItems.selectedArray = fourFourArray;
-                gyroItems.state = gyroItems.selectedArray[0];
-            }
-        }, 2000);
-    });
+  $(".conduct-btn").click(function() {
+      setTimeout(function() {
+          if (!conduct) {
+              conduct = true;
+              myo.on('gyroscope',function(data) {
+                  setTimeout(gyroRepeat(data),10);
+              });
+          } else {
+              conduct = false;
+              myo.off();
+              gyroItems.prevState;
+              gyroItems.receivedBeat;
+              gyroItems.beatNumber = 0;
+              gyroItems.selectedArray = fourFourArray;
+              gyroItems.state = gyroItems.selectedArray[0];
+          }
+      }, 2000);
+  });
 }
 try {
-    Myo.on('connected', function(){  
-        myMyo = this;
-        addEvents(myMyo);
-    });
-
-    Myo.connect();
+  Myo.on('connected', function(){  
+      myMyo = this;
+      addEvents(myMyo);
+  });
+  Myo.connect();
 } catch (err) {
-    //cannot use Myo.
+  //cannot use Myo.
+  console.log("cannot use Myo");
+  console.log(err);
 }
-
-
-
-
-
-
-
-
-
 
 $(document).ready(function(){
 	$(".dial").knob({
@@ -121,13 +123,20 @@ $(document).ready(function(){
     console.log(pushOntoMe);
     var timestamps = pushOntoMe;
 
+    var sum = 0;
+    for (var i=0; i<timestamps.length; i++) {
+      sum += timestamps[i];
+    }
+
+    sum = -sum;
+
     //var timestamps = [16,23,36,45,23,64,13,09,88,23,75,82,79,85,78,29,37,48,97,23,98,49,72,73,89,47,28,93,79];
     var userBeat = timestamps.map(function(val,index) {
       return 60000/(-val) < 200 ? {index:index+1, BPM:60000/(-val)} : {index:index, BPM:200};
     }); // variable BPM; val is delta [i]-[i+1]
     var baseBeat = timestamps.map(function(val,index) {return {index:index+1, BPM:tempo};}); // all constant BPM
 
-    var margin = {top: 40, right: 20, bottom: 30, left: 50},
+    var margin = {top: 80, right: 20, bottom: 30, left: 50},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
@@ -205,10 +214,17 @@ $(document).ready(function(){
     // Title
     svg.append("text")
         .attr("x", (width / 2))             
+        .attr("y", 0 - (margin.top / 2) - 24)
+        .attr("text-anchor", "middle")
+        .style("font-size", "18px") 
+        .text("Trial #" + trials + ": " + timestamps.length + " beats");
+
+    svg.append("text")
+        .attr("x", (width / 2))             
         .attr("y", 0 - (margin.top / 2))
         .attr("text-anchor", "middle")
         .style("font-size", "18px") 
-        .text("Trial #" + trials + ": " + timestamps.length + " beats at " + tempo + " beats per minute");
+        .text("Attempted: " + tempo + " beats per minute; Actual: " + (Math.round(100*timestamps.length * 60000 / sum,2)/100) + " beats per minute");
 
     trials++;
 
