7f9ad9b00fff67245f25a36065365ee94859492d
Kevin Kang <kkang@berkeley.edu>
2016-02-13 18:20:21 -0800

    finished prototype of myo conducting mechanism

--- myNewMyoApp.py
+++ myNewMyoApp.py
@@ -1,4 +1,5 @@
 import myo as libmyo
+import time
 from vars import myPath
 libmyo.init(myPath)
 feed = libmyo.device_listener.Feed()
@@ -10,20 +11,67 @@ try:
         print("No Myo connected after 10 seconds.")
         sys.exit()
     if hub.running and myo.connected:
+        orignalAngVel = myo.gyroscope
+        goingUp = True
+        downbeatDiff = 0
+        """
         quat = myo.orientation
         firstX = quat.x
         firstY = quat.y
         firstZ = quat.z
-        YReset = False
+        xDown = False
+        xUp = False
+        yLeft = False
+        yRight = False
+        zLeft = False
+        zRight = False
+        print("Orientation:", round(quat.x, 3), round(quat.y, 3), round(quat.z, 3), round(quat.w, 3))
+        """
     while hub.running and myo.connected:
+        angVel = myo.gyroscope
+        accel = myo.acceleration
+        time.sleep(1)
+        print("angular Velocity: ", round(angVel.z,1))  
+        print("acceleration: ", round(accel.y, 2)) 
+        
+        downbeatDiff = angVel.x
+        """
         quat = myo.orientation
-        if (quat.x - firstX > 0.2 and not YReset):
-            YReset = True
-            print("down")
-        if (quat.x - firstX < 0.2 and YReset):
-            YReset = False
-            print("reset")
-        #print("Orientation:", round(quat.w, 3), round(quat.y, 3), round(quat.z, 3))
+        //below is code based on quaternion
+        if (abs(quat.x - firstX) > 0.2) and (not xDown) and (not xUp):
+            if (quat.x - firstX < 0.2):  
+                xDown = True
+                xUp = False
+                print("Orientation:", round(quat.x, 3), round(quat.y, 3), round(quat.z, 3), round(quat.w, 3))
+                print("down")
+            else:
+                xUp = True
+                xDown = False
+                print("Orientation:", round(quat.x, 3), round(quat.y, 3), round(quat.z, 3), round(quat.w, 3))
+                print("up")
+        if (abs(quat.x - firstX) < 0.2) and (xDown or xUp):
+            xDown = False
+            xUp = False
+            print("Orientation:", round(quat.x, 3), round(quat.y, 3), round(quat.z, 3), round(quat.w, 3))
+            print("vertical centered")
+        if (abs(quat.z - firstZ) > 0.2) and (not zLeft) and (not zRight):
+            if (quat.z - firstZ < 0.2):  
+                zLeft = True
+                zRight = False
+                print("Orientation:", round(quat.x, 3), round(quat.y, 3), round(quat.z, 3), round(quat.w, 3))
+                print("left")
+            else:
+                zRight = True
+                zLeft = False
+                print("Orientation:", round(quat.x, 3), round(quat.y, 3), round(quat.z, 3), round(quat.w, 3))
+                print("right")
+        if (abs(quat.z - firstZ) < 0.2) and (zLeft or zRight):
+            zLeft = False
+            zRight = False
+            print("Orientation:", round(quat.x, 3), round(quat.y, 3), round(quat.z, 3), round(quat.w, 3))
+            print("horiz centered")
+        #print("Orientation:", round(quat.x, 3), round(quat.y, 3), round(quat.z, 3), round(quat.w, 3))
+        """
 except KeyboardInterrupt:
     print("Quitting...")
 finally:
